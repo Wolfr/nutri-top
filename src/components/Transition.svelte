38520d600a70a5fc7cad5921c9f6673bd936841e
Johan Ronsse <johan.ronsse@gmail.com>
2020-05-29 22:36:32 +0200

    Add a custom transitiom

--- src/components/Transition.svelte
+++ src/components/Transition.svelte
@@ -1,7 +1,24 @@
 <script>
-  import { scale, fly } from 'svelte/transition'
   import { BaseTransition } from "@sveltech/routify/decorators"
   import { cubicInOut } from 'svelte/easing';
+  
+  export function flyCustom(node, {
+      delay = 0,
+      duration = 400,
+      easing = cubicOut,
+      x = 0,
+      y = 0
+  }) {
+      const style = getComputedStyle(node);
+      const transform = style.transform === 'none' ? '' : style.transform;
+      return {
+          delay,
+          duration,
+          easing,
+          css: (t, u) => `transform: ${transform} translate(${(1 - t) * x}px, ${(1 - t) * y}px);`
+      };
+  }
+
   export let scoped
   const { width } = scoped
   const configs = [
@@ -11,15 +28,15 @@
     },
     {
       condition: c => c.toDescendant,
-      transition: fly,
-      inParams: { x: $width, duration: 250, easing: cubicInOut },
-      outParams: { x: -$width, duration: 250, easing: cubicInOut },
+      transition: flyCustom,
+      inParams: { x: $width, duration: 600, easing: cubicInOut },
+      outParams: { x: -$width, duration: 600, easing: cubicInOut },
     },
     {
       condition: c => c.toAncestor,
-      transition: fly,
-      inParams: { x: -$width, duration: 250, easing: cubicInOut },
-      outParams: { x: $width, duration: 250, easing: cubicInOut },
+      transition: flyCustom,
+      inParams: { x: -$width, duration: 600, easing: cubicInOut },
+      outParams: { x: $width, duration: 600, easing: cubicInOut },
     },
     {
       // No matching config. We don't want a transition
